5ea827f7da9ee363f636b36b75095e1a94fb311a
Jaela Wesley <jcwesley93@gmail.com>
2019-02-05 03:38:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   sentence_array = []
   array = sentence.split
-  array.each do |x|
-    x.reverse
-    sentence_array << x
+  array.each do |word|
+    x.reverse_inplace
+    sentence_array << word
   end
   sentence_array.join(" ")
 end
